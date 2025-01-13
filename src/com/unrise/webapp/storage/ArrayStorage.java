b6cdcdb4b2b8387056bb05eada5c04fa608ad3ee
LenaLaRoux <lena.laroux@gmail.com>
2025-01-13 16:02:01 +0500

    topjava forth lesson first check

--- src/com/unrise/webapp/storage/ArrayStorage.java
+++ src/com/unrise/webapp/storage/ArrayStorage.java
@@ -11,7 +11,7 @@ public class ArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void processSave(Resume resume, int index) {
-        storage[size + 1] = resume;
+        storage[size] = resume;
     }
 
     @Override
