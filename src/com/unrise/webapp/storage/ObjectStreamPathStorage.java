8f11d9b59bfd7fa02aa2ff1e6777e0e9e1585549
LenaLaRoux <lena.laroux@gmail.com>
2025-03-06 01:17:40 +0500

    topjava lesson 10 remove

--- src/com/unrise/webapp/storage/ObjectStreamPathStorage.java
+++
@@ -1,7 +0,0 @@
-package com.unrise.webapp.storage;
-
-public class ObjectStreamPathStorage extends AbstractPathStorage {
-    protected ObjectStreamPathStorage(String directory) {
-        super(directory, new ObjectStreamWRStrategy());
-    }
-}
